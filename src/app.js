4961b80aa6d9e958338555aa538fa6c3a7786eeb
Juan Camilo Sierra Rojas <camilo.sierra@co.onelink.bpo>
2022-11-17 14:30:25 -0500

    products seeders new database

--- src/app.js
+++ src/app.js
@@ -19,10 +19,10 @@ const imagesRouter = require("./images/images.router");
 const app = express();
 
 //? Seeders functions
-const createCategories = require("./utils/seeders/categories");
-const createImagesPacks = require("./utils/seeders/images_packs");
-const createImages = require("./utils/seeders/images");
-const createUsers = require("./utils/seeders/users");
+// const createCategories = require("./utils/seeders/categories");
+// const createImagesPacks = require("./utils/seeders/images_packs");
+// const createImages = require("./utils/seeders/images");
+// const createUsers = require("./utils/seeders/users");
 const createProducts = require("./utils/seeders/products");
 const whitelist = [
   "http://127.0.0.1:9000",
@@ -90,8 +90,8 @@ app.listen(config.port, () => {
 });
 
 //? Seeders execution
-createCategories(db);
-createImagesPacks(db);
-createImages(db);
-createUsers(db);
+// createCategories(db);
+// createImagesPacks(db);
+// createImages(db);
+// createUsers(db);
 createProducts(db);
--- src/utils/seeders/products.js
+++ src/utils/seeders/products.js
@@ -7,6 +7,7 @@ const createProducts = () => {
       title: "Samsung Galaxy S22",
       description:
         "Smartphone, Factory Unlocked Android Cell Phone, 256GB, 8K Camera & Video, Brightest Display, Long Battery Life, Fast 4nm Processor, US Version. 8K SUPER STEADY VIDEO: Shoot videos that rival how epic your life is with stunning 8K recording, the highest recording resolution available on a smartphone; Video captured is effortlessly smooth, thanks to Auto Focus Video Stabilization on Galaxy S22*.",
+      brand: "Sansung",
       price: 850.0,
       imagesPackId: "830e6358-8e0f-4d19-b74d-90af55696a1b",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
@@ -17,6 +18,7 @@ const createProducts = () => {
       title: "Samsung 32 inches QLED 4K Smart TV",
       description:
         "4K UHD Processor: a powerful processor optimizes your tv’ s performance with 4K picture quality Inputs & Outputs: 3 HDMI ports, 1 Ethernet port, 2 USB Ports (v 2.0), 1 Digital Audio Output (Optical), 1 Composite Input (AV), Dimensions with Stand | without Stand Weight: 11.7 lb. 4K UHD: see what you’ve been missing on a crisp, clear picture that’s 4x the resolution of Full HD Smart TV with Universal Guide: our simple on-screen Guide is an easy way to find streaming content and live TV showsHD: see shades of color that reveal more detail than HDTV can deliver.",
+      brand: "Samsung",
       price: 500.0,
       imagesPackId: "04735dc8-d14f-41a6-b541-eed5e433af6a",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
@@ -27,6 +29,7 @@ const createProducts = () => {
       title: "BRAVIA 4K HDR Full Array LED",
       description:
         "COGNITIVE PROCESSOR XR™ – Revolutionary TV processing technology that understands how humans see and hear to deliver intense contrast with deep blacks, high peak brightness, and natural colors. Rediscover everything you watch with billions of accurate colors and see impressive picture quality that is natural and beautiful to the human eye. Precisely controlled backlighting brings out real-life depth and detail with deep blacks and high peak brightness.",
+      brand: "Bravia",
       price: 1099.0,
       imagesPackId: "4fdee44b-9ae1-4573-bb8f-26f9c6f181df",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
@@ -37,6 +40,7 @@ const createProducts = () => {
       title: "2021 Apple MacBook Pro",
       description:
         "Apple M1 Pro or M1 Max chip for a massive leap in CPU, GPU, and machine learning performance. Up to 10-core CPU delivers up to 3.7x faster performance to fly through pro workflows quicker than ever. Up to 32-core GPU with up to 13x faster performance for graphics-intensive apps and games. 16-core Neural Engine for up to 11x faster machine learning performance. Longer battery life, up to 17 hours. Up to 64GB of unified memory so everything you do is fast and fluid.",
+      brand: "Apple",
       price: 1300.0,
       imagesPackId: "4522b3f5-a634-44e5-8416-dcb6cc6ee318",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
@@ -47,6 +51,7 @@ const createProducts = () => {
       title: "Sony X85J 55 Inch smart TV",
       description:
         "4K Ultra HD LED Smart Google TV with Native 120HZ Refresh Rate, Dolby Vision HDR and Alexa Compatibility KD55X85J- 2021 Model,Black. 4K HDR PROCESSOR X1 – Powerful TV processing that delivers a picture that is smooth and clear, full of rich colors and detailed contrast. Reproduces more colors than a conventional TV resulting in picture quality that is natural and precise, and closer than ever to real life. Seamlessly browse 700,000+ movies and TV episodes from across your favorite streaming services, all in one place.",
+      brand: "Sony",
       price: 1198.0,
       imagesPackId: "9624d5dc-b3c3-4e22-a7da-6c42fa4137a7",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
@@ -57,6 +62,7 @@ const createProducts = () => {
       title: "Pocophone X3 Pro",
       description:
         "This is the first POCO phone equipped with a 6nm chipset and powered by four super performance Arm Cortex-A78 cores which effectively balance between performance and energy consumption. With UFS 3.1, you can enjoy shorter loading times whether you are gaming or taking pictures. The multiple high-efficiency heat dissipation materials create flagship-level temperature control and rapid cooling. LiquidCool Technology 2.0 allows the phone to be in a high-performance state at any time, unleashing the maximum potential of the processor.",
+      brand: "Xiaomi",
       price: 275.0,
       imagesPackId: "908121d1-ea3c-4def-806f-2c717288e903",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
@@ -67,6 +73,7 @@ const createProducts = () => {
       title: "Samsung 50 inches 4K Ultra HD Smart LED TV",
       description:
         "Samsung UN50TU7000 50 TU7000 QLED 4K UHD Smart TV (2020 Model) The TU7000 Crystal UHD TV unlocks hidden detail at four times the quality of Full HD. The ultra-fast processor transforms everything you watch into stunning 4K. The sleek, elegant, and minimalistic design draws you into the screen from any angle. UNDLE INCLUDES: Deco Home 31 Home Theater Premium Soundbar | Deco Mount Flat & Tilt Wall Mount for TV's.",
+      brand: "Samsung",
       price: 570.0,
       imagesPackId: "66fa90c6-0146-42bb-a8ba-a54a9256c436",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
@@ -77,6 +84,7 @@ const createProducts = () => {
       title: "SAMSUNG Galaxy Book Pro",
       description:
         "Book Pro Intel Evo Platform Laptop Computer 15.6 inches AMOLED Screen 11th Gen Intel Core i7 Processor 16GB Memory 512GB SSD Long-Lasting Battery, Mystic Blue. EASY ON THE EYES: Everything looks brilliant with the first 13.3/15.6 AMOLED screen in a Galaxy Book; Enjoy vibrant viewing while you work or school inside or in direct sunlight, and it lessens eye strain by reducing harmful blue light. Sleek, ultrathin and available in a stylish, dual color design, Galaxy Book Pro is the lightest laptop and won’t weigh you down.",
+      brand: "Samsung",
       price: 1050.0,
       imagesPackId: "be6f22ed-ad9f-4dba-bf1a-ecf36a0d7b2c",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
@@ -87,6 +95,7 @@ const createProducts = () => {
       title: "iPhone 13 Pro",
       description:
         "6.1-inch Super Retina XDR display with ProMotion for a faster, more responsive feel. Cinematic mode adds shallow depth of field and shifts focus automatically in your videos. Pro camera system with new 12MP Telephoto, Wide, and Ultra Wide cameras; LiDAR Scanner; 6x optical zoom range; macro photography; Photographic Styles, ProRes video, Smart HDR 4, Night mode, Apple ProRAW, 4K Dolby Vision HDR recording. 12MP TrueDepth front camera with Night mode, 4K Dolby Vision HDR recording. A15 Bionic chip for lightning-fast performance.",
+      brand: "Apple",
       price: 1300.0,
       imagesPackId: "0abb52fa-bc4d-498d-aafe-2121764200f2",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
@@ -97,6 +106,7 @@ const createProducts = () => {
       title: "iPhone 12",
       description:
         "A superpowerful chip. 5G speed. An advanced dual‑camera system. A Ceramic Shield front that's tougher than any smartphone glass. And a bright, beautiful OLED display. iPhone 12 has it all — in two great sizes. Our hardware and software work together seamlessly. Want to pair new AirPods with your iPhone? It’s a simple one‑tap setup. Want to share photos, videos, or contacts with friends nearby?",
+      brand: "Apple",
       price: 600.0,
       imagesPackId: "4b691476-0300-461e-96c0-fd03a6e0b015",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
@@ -107,6 +117,7 @@ const createProducts = () => {
       title: "2021 Apple iMac",
       description:
         "Immersive 24-inch 4.5K Retina display with P3 wide color gamut and 500 nits of brightness, 24-inch, Apple M1 chip with 8‑core CPU and 8‑core GPU, 8GB RAM, 256GB. Apple M1 chip delivers powerful performance with 8-core CPU and 8-core GPU, Strikingly thin 11.5 mm design in vibrant colors. 1080p FaceTime HD camera with M1 ISP for amazing video quality. Studio-quality three-mic array for crystal clear calls and voice recordings. Six-speaker sound system for a remarkably robust and high-quality audio experience.",
+      brand: "Apple",
       price: 1500.0,
       imagesPackId: "ae3a08ae-9d28-478c-8768-67506fa340a1",
       userId: "39bbcef5-bff2-4ea2-b0af-6a3b2c08fec9",
